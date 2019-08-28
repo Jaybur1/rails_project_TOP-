94cc1c638132e970d2417ee31f30bac300cfe273
Jay Burbyga <jayburbyga@gmail.com>
2019-08-28 17:58:16 -0400

    Added full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+
+  #returns the title on a per-page besis
+  def full_title(page_title = '')
+    base_title = 'RoR tutorial sample app'
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title 
+  end
 end
